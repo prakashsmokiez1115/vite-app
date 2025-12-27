c341daf278e8c89e3571aed8535bea9fe9d4e271
prakash <prakashsmokiez1115@gmail.com>
2025-12-27 18:52:18 +0530

    dec-27 vite app changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { createContext, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -9,10 +9,14 @@ import State from './State'
 import ListKeys from './ListKeys'
 
 
+export let userData = createContext();
+
 function App() {
 
-  const user1 = "Sethupathi";
-  const user2 = "Prakash";
+  const user1 = "Prakash";
+  const user2 = "Sethu";
+  const user3 = "Shivash";
+  const user4 = "Meiarasu";
 
   return (
     <div>
@@ -29,7 +33,11 @@ function App() {
 
       {/* <State /> */}
 
-      <ListKeys />
+      {/* <ListKeys /> */}
+
+      <userData.Provider value={{ user1, user2, user3, user4 }}>
+        <Child1 />
+      </userData.Provider>
     </div>
   )
 }
--- src/Child3.jsx
+++ src/Child3.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { userData } from './App'
+
+
 
 const Child3 = (c3) => {
+
+    let getData3 = useContext(userData);
+
     return (
         <div>
             <u><h1>Child-3</h1></u>
             <h2>{c3.user1}</h2>
             <h2>{c3.user2}</h2>
+
+            <ul style={{ listStyle: "square" }}>
+                <li>{getData3.user1}</li>
+                <li>{getData3.user2}</li>
+                <li>{getData3.user3}</li>
+                <li>{getData3.user4}</li>
+            </ul>
+
         </div>
     )
 }
